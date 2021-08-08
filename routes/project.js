7ef2fd3353ef2706de2c6b90acd458ad83fef0bd
Kattya Herrera Salazar <kattya.herrera@sansano.usm.cl>
2021-08-08 18:16:43 -0400

    creacion proyecto con error

---
+++ routes/project.js
@@ -0,0 +1,31 @@
+var express = require('express');
+var router = express.Router();
+const fetch = require("node-fetch");
+
+router.post('/new', async function(req, res, next) {
+    //const proyecto = await Proyecto.create(req.body);
+    //res.json(proyecto)
+
+    const options = {method: 'GET', headers: {Accept: 'application/json'}};
+
+    let personVerification = await fetch('http://ec2-3-13-79-51.us-east-2.compute.amazonaws.com:8081/assistant/rut?rut='+req.body.id_maker, options);
+    
+    if(personVerification.status == 200){
+ 
+        const create = {method: 'POST', headers: {Accept: 'application/json'}, body: req.body};
+        let projectCreation = await fetch('https://api-gestion-production-fob3.up.railway.app/proyectos/', create);
+ 
+        if(projectCreation.status==200){
+            res.json(projectCreation)
+        }else{
+            console.log(projectCreation)
+            res.status(404).send({failed: "Error creando proyecto"})
+        }
+        
+
+    }else{
+        res.status(404).send({failed: "No existe la persona indicada"})
+    }
+});
+
+module.exports = router;
